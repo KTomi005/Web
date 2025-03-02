aadcfbc7bde6784744ac466f5508907da190632a
KTomi005 <kemtam197@hengersor.hu>
2025-03-02 21:49:48 +0100

    Text update

--- src/KezdoLap/KezdoLap.jsx
+++ src/KezdoLap/KezdoLap.jsx
@@ -15,8 +15,10 @@ const KezdoLap = () => {
             <br />
             <div className="Informational">
                 <h3>Informational data</h3>
-                <p>Welcome to our first game project. Its name is Dungeon Explorer. This is a graphically poor game. We are a three-membered group. So don't expect such a good game at first :)</p>
+                <div className="info-block">
+                <p>Welcome to our very first game project, Dungeon Explorer! We are thrilled to share this adventure with you, even though it's a work in progress. As a small, three-member team, we are still learning and improving with each step. Please keep in mind that the graphics might not be top-notch yet, and the game might not be as polished as bigger, more developed projects. But what we lack in visuals and perfection, we make up for with passion, creativity, and a desire to provide a fun and engaging experience.</p>
             </div>
+        </div>
         </div>
     );
 };
